03a8188030de95eadb5413d89dd2448b2cded102
Aaryna Michelle Irwin <irwind5@mail.tmcc.edu>
2017-04-05 02:50:22 -0700

    began working on driver code, general cleanup

--- poly_class.cpp
+++ poly_class.cpp
@@ -20,6 +20,9 @@ Aaryna Irwin            2017-03-17         0.1 - Initial
 #include <iostream>
 #include <iomanip>
 
+#define F_1 setw(30)
+#define F_2 setw(15)
+
 /* -----------------------------------------------------------------------------
 FUNCTION:          main(const int, const char**)
 DESCRIPTION:       There your function.
@@ -28,20 +31,41 @@ NOTES:             None
 ----------------------------------------------------------------------------- */
 int main(const int argc, const char* argv[])
 {
+	using std::cout;
+	using std::endl;
+	using std::setw;
+
     Polynomial poly[3];
 
     for (int n = 0; n < 2; ++n)
     {
 		std::cin >> poly[n];
     }
-	
-	std::cout << std::right
-		<< std::setw(30) << "The first polynomial is: " << poly[0] << std::endl
-		<< std::setw(30) << "The second polynomial is: " << poly[1] << std::endl
-		<< std::setw(30) << "Their sum is: " << poly[0] + poly[1] << std::endl;
+
+	cout << std::right
+		<< F_1 << "The first polynomial is: " << F_2 << poly[0] << endl
+		<< F_1 << "The second polynomial is: " << F_2 << poly[1] << endl
+		<< endl;
+
+	// Demonstrate addition and compound addition assignment
+	cout << F_1 << "Their sum is: " << F_2 << poly[0] + poly[1] << endl;
 	poly[2] = poly[0];
 	poly[2] += poly[1];
-	std::cout << std::setw(30) << "Compound operation: " << poly[2] << std::endl;
+	cout << F_1 << "Compound operation: " << F_2 << poly[2] << endl	<< endl;
+
+	// Demonstrate subtraction and compound subtraction assignment
+	cout << F_1 << "Their difference is: " << F_2 << poly[0] - poly[1]
+		<< endl;
+	poly[2] = poly[0];
+	poly[2] -= poly[1];
+	cout << F_1 << "Compound operation: " << F_2 << poly[2] << endl	<< endl;
+
+	// Demonstrate multiplication and compound multiplication assignment
+	cout << F_1 << "Their product is: " << F_2 << poly[0] * poly[1]
+		<< endl;
+	poly[2] = poly[0];
+	poly[2] *= poly[1];
+	cout << F_1 << "Compound operation: " << F_2 << poly[2] << endl	<< endl;
 
     return 0;
 }
