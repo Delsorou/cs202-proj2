2a8713b0c60b9255ddaa7a20a137f8b7d9d6d037
Aaryna Michelle Irwin <irwind5@mail.tmcc.edu>
2017-04-06 00:32:36 -0700

    fixes #15 completed base driver

--- poly_class.cpp
+++ poly_class.cpp
@@ -21,7 +21,6 @@ Aaryna Irwin            2017-03-17         0.1 - Initial
 #include <iomanip>
 
 #define F_1 setw(30)
-#define F_2 setw(15)
 
 /* -----------------------------------------------------------------------------
 FUNCTION:          main(const int, const char**)
@@ -35,37 +34,68 @@ int main(const int argc, const char* argv[])
 	using std::endl;
 	using std::setw;
 
-    Polynomial poly[3];
+	// Demonstrate default and overloaded constructors
+	cout << "Enter the degree and coefficients for the polynomial f(x): ";
+	Polynomial* Poly0 = new Polynomial(std::cin);
+	cout << endl;
+	cout << "Enter the degree and coefficients for the polynomial g(x): ";
+	Polynomial* Poly1 = new Polynomial(std::cin);
+	cout << endl << endl;
+	Polynomial* Poly2 = new Polynomial();
 
-    for (int n = 0; n < 2; ++n)
-    {
-		std::cin >> poly[n];
-    }
-
-	cout << std::right
-		<< F_1 << "The first polynomial is: " << F_2 << poly[0] << endl
-		<< F_1 << "The second polynomial is: " << F_2 << poly[1] << endl
-		<< endl;
+	cout << std::right << F_1 << "f(x) = " <<  *Poly0 << endl
+		<< F_1 << "g(x) = " <<  *Poly1 << endl << endl;
 
 	// Demonstrate addition and compound addition assignment
-	cout << F_1 << "Their sum is: " << F_2 << poly[0] + poly[1] << endl;
-	poly[2] = poly[0];
-	poly[2] += poly[1];
-	cout << F_1 << "Compound operation: " << F_2 << poly[2] << endl	<< endl;
+	cout << F_1 << "(f + g)(x) = " << *Poly0 + *Poly1 << endl;
+	*Poly2 = *Poly0;
+	*Poly2 += *Poly1;
+	cout << F_1 << "(using +=): " << *Poly2 << endl << endl;
 
 	// Demonstrate subtraction and compound subtraction assignment
-	cout << F_1 << "Their difference is: " << F_2 << poly[0] - poly[1]
-		<< endl;
-	poly[2] = poly[0];
-	poly[2] -= poly[1];
-	cout << F_1 << "Compound operation: " << F_2 << poly[2] << endl	<< endl;
+	cout << F_1 << "(f - g)(x) = " << *Poly0 - *Poly1 << endl;
+	*Poly2 = *Poly0;
+	*Poly2 -= *Poly1;
+	cout << F_1 << "(using -=): " << *Poly2 << endl << endl;
 
 	// Demonstrate multiplication and compound multiplication assignment
-	cout << F_1 << "Their product is: " << F_2 << poly[0] * poly[1]
-		<< endl;
-	poly[2] = poly[0];
-	poly[2] *= poly[1];
-	cout << F_1 << "Compound operation: " << F_2 << poly[2] << endl	<< endl;
+	cout << F_1 << "(f * g)(x) = " << *Poly0 * *Poly1 << endl;
+	*Poly2 = *Poly0;
+	*Poly2 *= *Poly1;
+	cout << F_1 << "(using *=): " << *Poly2 << endl << endl;
 
-    return 0;
+	// Demonstrate equality and inequality
+	bool isEqual = *Poly0 == *Poly1;
+   	bool isInequal = *Poly0 != *Poly1;
+
+	cout << F_1 << "f(x) = g(x): " << std::boolalpha << isEqual << endl;
+	cout << F_1 << "f(x) != g(x): " << std::boolalpha << isInequal << endl;
+
+	// Demonstrate copy construction
+	cout << F_1 << "F(x) = f(x)  " << endl;
+	Polynomial* eqTest = new Polynomial(*Poly0);
+	
+	// Yep, equality and inequality totes work, yo!
+	isEqual = *Poly0 == *eqTest;
+   	isInequal = *Poly0 != *eqTest;
+
+	cout << F_1 << "F(x) = f(x): " << std::boolalpha << isEqual << endl;
+	cout << F_1 << "F(x) != f(x): " << std::boolalpha << isInequal
+		<< std::noboolalpha << endl << endl;
+
+	// Demonstrate evaluation
+	cout << F_1 << "Please define: x = ";
+	int value = 0;
+	std::cin >> value;
+	cout << endl << endl;
+
+	cout << F_1 << "f(x) = " << (*Poly0)(value) << " at x = " << value << endl;
+	cout << F_1 << "g(x) = " << (*Poly1)(value) << " at x = " << value << endl;
+
+	delete Poly0;
+	delete Poly1;
+	delete Poly2;
+   	delete eqTest;
+
+	return 0;
 }
